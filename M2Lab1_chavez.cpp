a9214beb8d09a9d433aa4426d67e43dc213a1b1f
RubenChavez-FTCC <chavezr3400@student.faytechcc.edu>
2026-09-16 14:35:23 +0000

    m2lab1 inputs

---
+++ M2Lab1_chavez.cpp
@@ -0,0 +1,49 @@
+//CSC 134
+//M2lab1 crates
+//Chavez, ruben
+//9/16/2026
+//excrise 3.11 from Gaddis
+
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+
+    // Declare const and varis
+
+
+
+    const double COST_PERCUBIC_FOOT = 0.23;
+    const double CHARGE_PERCUBIC_FOOT = 0.50;
+
+    double length, width, height; //can declare multiple 
+    double crate_cost;
+    double crate_charge;
+    double profit;
+    
+
+    //get the dimension from user
+    cout << "Please enter the crate dimensions " << endl;
+    cout << "Crate length: ";
+    cin >> length;
+    cout << "Crate width: ";
+    cin >> width;
+    cout << "Crate height: ";
+    cin >> height;
+
+
+    // calc the vol, cost, charge, and profit
+
+
+
+    //format and display it for user
+
+
+
+
+    return 0; //no errors 
+
+}
